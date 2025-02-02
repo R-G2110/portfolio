2c34cffe80a0c8496d88ec5bf939e9b2eee6050c
R-G2110 <argeldelacruz86@gmail.com>
2025-02-02 20:52:22 +0100

    [edit] object mail

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -8,7 +8,7 @@ export default {
   methods: {
     sendEmail() {
       const email = "argeldelacruz86@gmail.com";
-      const subject = encodeURIComponent("Hello Argel!");
+      const subject = encodeURIComponent("Let's Connect!");
       const body = encodeURIComponent("Hi Argel, I would like to get in touch with you.");
       const mailtoLink = `mailto:${email}?subject=${subject}&body=${body}`;
       
