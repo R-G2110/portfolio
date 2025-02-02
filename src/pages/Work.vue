3c91c675c38ee4bf32691a9c520b843ba618b838
R-G2110 <argeldelacruz86@gmail.com>
2025-02-02 15:46:50 +0100

    [edit] work page

--- src/pages/Work.vue
+++ src/pages/Work.vue
@@ -22,36 +22,33 @@ export default {
 
 <template>
   <main>
-    <!-- <div class="text">sorry,</div>
-    <div class="text">i'm still</div>
-    <div class="text">
-      working on it<span class="dots">{{ dots }}</span>
-    </div> -->
+    <div class="container">
+      <h1>work in progress...</h1>
+      <p>My bad, I'm still working on it.</p>
+
+    </div>
   </main>
 </template>
 
 <style lang="scss" scoped>
-main {
-  height: calc(100vh - 200px);
-  display: flex;
+.container {
+  width: 800px;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
 }
 
-.text {
+h1 {
+  width: fit-content;
   font-size: var(--font-xxxxl);
-  font-weight: bold;
-  text-align: center;
-  text-transform: uppercase;
-  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
-  line-height: 1;
-  margin-bottom: 20px;
+  border-bottom: var(--border-width) solid var(--border-color);
+  text-transform: capitalize;
+  text-shadow: 3px 3px 6px rgba(0, 0, 0, 0.3);
 }
 
-.dots {
-  display: inline-block;
-  width: 24px; 
-  text-align: left;
+p {
+  padding: 5px 15px;
+  margin-bottom: 50px;
+  color: var(--text-color);
+  text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.2);
 }
 </style>
